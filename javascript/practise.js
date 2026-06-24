e1fa30d4d8583ab02585f22cceda7cdb21723d58
lukman2458 <@lukman2458.com>
2026-06-24 14:26:44 +0530

    Practise javascript data types and conditionals
    I learned increments , if else , nested if`s , terinary ,multi-line and logical operators

--- javascript/practise.js
+++ javascript/practise.js
@@ -1,7 +1,92 @@
-console.log("hello world");
-let a = 10;
-console.log(a);
-let savedFirst = a++;
-console.log(savedFirst);
-let addedFirst = ++a;
-console.log(addedFirst);
+// ------------Pre and post increment-------------
+// console.log("hello world");
+// let a = 10;
+// console.log(a);
+// let savedFirst = a++;
+// console.log(savedFirst);
+// let addedFirst = ++a;
+// console.log(addedFirst);
+
+
+// ---------------if else--------------
+// let name = prompt("Whats my name");
+// if (name == "Lukman"){
+//     alert("Yes its true");
+// }
+// else{
+//     alert("Um,you dont know my name , Iam Lukman");
+// }
+
+// ----------If else else if--------------
+// let num = prompt("Say a number")
+// if (+num > 0){
+//     alert("1")
+// }
+// else if (+num < 0){
+//     alert("-1")
+// }
+// else{
+//     alert("0")
+// }
+
+// -----------Nested if
+// let login = prompt("Who are you");
+// if (login == "Admin"){
+//     let password = prompt("Whats the password")
+//         if (password == "suii"){
+//             alert("Acess allowed")
+//         }
+//         else if (password == "" || password == null){
+//             alert("Cancelled")
+//         }
+//         else{
+//             alert("Acess denied")
+//         }
+// }
+// else if (login == "" || login == null){
+//     alert("Cancelled")
+// }
+// else {
+//     alert("i dont know you")
+// }
+
+// -----------------Ternary------------
+// let e = 4;
+// let b = 3;
+// let result = e+b<4 ? alert("below") : alert("above");
+
+// ------------------------Multiline ternary---------
+// let login = prompt("who are you");
+// let msg;
+// login == "director" ? msg = "Action" :
+// login == "officer" ? msg = "Roger" :
+// login == "student" ? msg = "Greetings" :
+// msg = "who the h are you";
+// alert(msg);
+
+// ---------------Logical operators--------
+// ------AND &&
+// let age = prompt("what is your age");
+// if (age>= 14 && age <= 90){
+//     alert("You can get in the war")
+// }
+// else{
+//     alert("You cant get in war")
+// }
+// -----NOT !
+// let age = prompt("what is your age");
+// if (!(age>= 14 && age <= 90)){
+//     alert("You cant get in war")
+// }
+// else{
+//     alert("You can get in the war")
+// }
+// ---------OR ||
+// let age = prompt("what is your age");
+// if (age < 14 || age > 90){
+//     alert("You cant get in war")
+// }
+// else{
+//     alert("You can get in the war")
+// }
+
